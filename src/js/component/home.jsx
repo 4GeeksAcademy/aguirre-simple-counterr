8db36f70be397062a124bab61b056a1b427eba4b
julian aguirre <julianaguirrew@gmail.com>
2023-11-09 19:37:20 +0000

    finished simple couunter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,21 +4,35 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+let first = Math.floor(props.counter / 1) % 10;
+let second = Math.floor(props.counter / 10) % 10;
+let third = Math.floor(props.counter / 100) % 10;
+let fourth = Math.floor(props.counter / 1000) % 10;
+let fifth = Math.floor(props.counter / 10000) % 10;
+let sixth = Math.floor(props.counter / 100000) % 10;
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center d-flex justify-content-center" >
+			<div className="rounded bg-black text-white me-2" style={{width:"7rem", height:"9rem", fontSize:"5rem"}}>
+				{sixth}
+			</div>
+			<div className="rounded bg-black text-white me-2" style={{width:"7rem", height:"9rem", fontSize:"5rem"}}>
+				{fifth}
+			</div>
+			<div className="rounded bg-black text-white me-2" style={{width:"7rem", height:"9rem", fontSize:"5rem"}}>
+				{fourth}
+			</div>
+			<div className="rounded bg-black text-white me-2" style={{width:"7rem", height:"9rem", fontSize:"5rem"}}>
+				{third}
+			</div>
+			<div className="rounded bg-black text-white me-2" style={{width:"7rem", height:"9rem", fontSize:"5rem"}}>
+				{second}
+			</div>
+			<div className="rounded bg-black text-white me-2" style={{width:"7rem", height:"9rem", fontSize:"5rem"}}>
+				{first}
+			</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,13 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0
+setInterval(incrementSeconds, 1000);
+function incrementSeconds() {
+    ReactDOM.render(<Home counter={counter}/>, document.querySelector("#app"));
+    counter += 1;
+ 
+}
+
+
+
